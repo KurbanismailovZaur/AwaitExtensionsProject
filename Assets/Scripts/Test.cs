645c08e19c2e520306a46d59a18acc48562b517a
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-09-01 05:22:31 +0300

    Added transform move extensions

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -13,68 +13,13 @@ namespace Numba
     public class Test : MonoBehaviour
     {
         [SerializeField]
-        private Transform _cube0;
-
-        [SerializeField]
-        private Transform _cube1;
-
-        [SerializeField]
-        private Ease _ease;
-
-        [SerializeField]
-        [Range(0f, 1f)]
-        private float _time;
-
-        [SerializeField]
-        private bool _reversed;
-
-        private Tweak _tweak;
-
-        private Tween _tween;
+        private Transform _cube;
 
         private async void Awake()
         {
-            //Tween xTween = new Tween("MyXTween", 0f, 1f, (x) => { SetXPosition(_cube0, x); }, 1f).SetEase(Ease.OutExpo).SetLoops(-1, LoopType.ReversedYoyo);
-            //xTween.PlayAsync();
+            await _cube.DoMove(1, 2, 3, 1, Ease.InOutSine, -1, LoopType.ReversedYoyo).PlayAsync();
 
-            //Tween yTween = new Tween("MyYTween", 1f, 0f, (x) => { SetXPosition(_cube1, x); }, 1f).SetEase(Ease.OutExpo).SetLoops(-1, LoopType.ReversedYoyo);
-            //yTween.PlayAsync();
-            //Tween yTween = new Tween("MyYTween", 0f, 2f, (y) => { SetYPosition(_cube, y); }, 2f).SetEase(Ease.CircIn).SetLoops(-1, LoopType.Yoyo);
-            //Tween zTween = new Tween("MyZTween", 0f, 3f, (z) => { SetZPosition(_cube, z); }, 3f).SetEase(Ease.QuintInOut).SetLoops(-1, LoopType.ReversedYoyo);
-
-            Sequence sequence = new Sequence();
-            sequence.Append(new Tween("Tween0", 0f, 1f, (x) => { SetXPosition(_cube0, x); }, 6f).SetEase(Ease.OutExpo).SetLoops(1, LoopType.ReversedYoyo));
-            sequence.Insert(0f, new Tween("Tween1", 0f, 1f, (x) => { SetXPosition(_cube1, x); }, 1f).SetLoops(-1, LoopType.Backward));
-
-            await sequence.SetLoopsCount(2).PlayAsync();
-
-            Log("Tweened!");
+            Log("Completed");
         }
-
-        private void Update()
-        {
-            //_tween.SetTime(_time);
-        }
-
-        private void SetXPosition(Transform transform, float xPosition)
-        {
-            var position = transform.position;
-            position.x = xPosition;
-            transform.position = position;
-        }
-
-        //private void SetYPosition(Transform transform, float yPosition)
-        //{
-        //    var position = transform.position;
-        //    position.y = yPosition;
-        //    transform.position = position;
-        //}
-
-        //private void SetZPosition(Transform transform, float zPosition)
-        //{
-        //    var position = transform.position;
-        //    position.z = zPosition;
-        //    transform.position = position;
-        //}
     }
 }
--- Assets/Scripts/Tweening/Extensions.cs
+++ Assets/Scripts/Tweening/Extensions.cs
@@ -3,16 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks;
 using static UnityEngine.Debug;
+using System;
 
 namespace Numba.Tweening
 {
     public static class Extensions
     {
-        //public static Task MoveAsync(this Transform transform, Vector3 position, float duration)
-        //{
-            
+        #region Transform
+        #region Move
+        #region Global
+        public static Sequence DoMove(this Transform transform, float x, float y, float z, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMove(transform, new Vector3(x, y, z), duration, ease, loopsCount, loopType);
 
+        public static Sequence DoMove(this Transform transform, Vector3 position, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMove((a) => transform.position[a], (a, v)=> SetAxisValue(transform, Space.World, a, v), position, duration, ease, loopsCount, loopType);
 
-        //}
+        public static Tween DoMoveX(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.position.x, (x) => SetAxisValue(transform, Space.World, 0, x), value, duration, ease, loopsCount, loopType);
+
+        public static Tween DoMoveY(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.position.y, (y) => SetAxisValue(transform, Space.World, 1, y), value, duration, ease, loopsCount, loopType);
+
+        public static Tween DoMoveZ(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.position.z, (z) => SetAxisValue(transform, Space.World, 2, z), value, duration, ease, loopsCount, loopType);
+        #endregion
+
+        #region Local
+        public static Sequence DoLocalMove(this Transform transform, float x, float y, float z, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoLocalMove(transform, new Vector3(x, y, z), duration, ease, loopsCount, loopType);
+
+        public static Sequence DoLocalMove(this Transform transform, Vector3 position, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMove((a) => transform.localPosition[a], (a, v) => SetAxisValue(transform, Space.Self, a, v), position, duration, ease, loopsCount, loopType);
+
+        public static Tween DoLocalMoveX(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.localPosition.x, (x) => SetAxisValue(transform, Space.Self, 0, x), value, duration, ease, loopsCount, loopType);
+
+        public static Tween DoLocalMoveY(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.localPosition.y, (y) => SetAxisValue(transform, Space.Self, 1, y), value, duration, ease, loopsCount, loopType);
+
+        public static Tween DoLocalMoveZ(this Transform transform, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => DoMoveAxis(() => transform.localPosition.z, (z) => SetAxisValue(transform, Space.Self, 2, z), value, duration, ease, loopsCount, loopType);
+        #endregion
+
+        private static Sequence DoMove(Func<int, float> axisValueGetter, Action<int, float> axisValueSetter, Vector3 position, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward)
+        {
+            Sequence sequence = new Sequence();
+            sequence.Insert(0f, new Tween(axisValueGetter(0), position.x, (x) => axisValueSetter(0, x), duration).SetEase(ease).SetLoopType(loopType));
+            sequence.Insert(0f, new Tween(axisValueGetter(1), position.x, (y) => axisValueSetter(1, y), duration).SetEase(ease).SetLoopType(loopType));
+            sequence.Insert(0f, new Tween(axisValueGetter(2), position.x, (z) => axisValueSetter(2, z), duration).SetEase(ease).SetLoopType(loopType));
+            sequence.LoopsCount = loopsCount;
+
+            return sequence;
+        }
+
+        private static Tween DoMoveAxis(Func<float> axisValueGetter, Action<float> axisValueSetter, float value, float duration, Ease ease = Ease.Linear, int loopsCount = 1, LoopType loopType = LoopType.Forward) => new Tween(axisValueGetter(), value, axisValueSetter, duration).SetEase(ease).SetLoops(loopsCount, loopType);
+        #endregion
+
+        private static void SetAxisValue(Transform transform, Space space, int axis, float value)
+        {
+            if (space == Space.World) transform.position = SetAxisValue(transform.position, axis, value);
+            else transform.localPosition = SetAxisValue(transform.localPosition, axis, value);
+        }
+        #endregion
+
+        private static Vector3 SetAxisValue(Vector3 vector3, int axis, float value)
+        {
+            vector3[axis] = value;
+            return vector3;
+        }
     }
 }
