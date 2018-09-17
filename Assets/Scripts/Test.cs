a286061419af864c99afecf1dfd61f1ade7674e3
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2018-09-17 14:22:19 +0300

    Tweening engine was removed. Now this repository for AwaitExtensions only

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -6,9 +6,6 @@ using static UnityEngine.Debug;
 using System.Threading;
 using UnityEngine.SceneManagement;
 using System;
-//using Numba.Tweening;
-using Numba.Tweening.Tweaks;
-using Numba.Tweening;
 using UnityEngine.UI;
 
 namespace Numba
@@ -23,20 +20,7 @@ namespace Numba
 
         private void Start()
         {
-            _cube.DoPositionX(1f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-            _cube.DoEulerAnglesZ(-90f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-            _cube.DoLocalScaleX(2f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-
-            _cube.GetComponent<MeshRenderer>().material.DoColor(Color.yellow, 1f, Ease.InOutExpo, 1, LoopType.ReversedYoyo).Play();
-
-            Camera.main.transform.DoPosition(-10f, 0f, 0f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-            Camera.main.transform.DoEulerAnglesY(90f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-            Camera.main.DoFieldOfView(30f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-            //Camera.main.DoRect(new Rect(0.25f, 0.25f, 0.75f, 0.75f), 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-
-            //_canvasGroup.DoAlpha(1f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play();
-
-            //Tween.Time.DoTimeScale(0f, 1f, Ease.InOutExpo, 1, LoopType.Yoyo).Play(true);
+            
         }
     }
 }
