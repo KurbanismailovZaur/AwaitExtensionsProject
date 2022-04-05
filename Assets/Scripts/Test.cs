657fcbef922dd6315db2705b633b44611d32e6b5
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-04-05 23:03:33 +0300

    Replace all project. Added assembly definition

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,26 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading.Tasks;
-using static UnityEngine.Debug;
-using System.Threading;
-using UnityEngine.SceneManagement;
-using System;
-using UnityEngine.UI;
 using Redcode.Awaiting;
-
-namespace Redcode
+public class Test : MonoBehaviour
 {
-    public class Test : MonoBehaviour
+    async void Start()
     {
-        [SerializeField]
-        private Transform _cube;
-
-        [SerializeField]
-        private CanvasGroup _canvasGroup;
-
-        private void Start()
-        {
-        }
+        await new WaitForSeconds(1);
+        print("Works!");
     }
 }
